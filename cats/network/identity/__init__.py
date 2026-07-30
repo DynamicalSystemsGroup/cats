720011aa5f89e97edef1dd950cad009e8546a37a
Joshua Jodesty <joshua.jodesty@gmail.com>
2026-07-30 03:00:21 -0400

    Add WebID bridge from node did:key
    feat(identity): emit minimal WebID profile linking Node did:key
     verification method; export webid_uri helpers

--- cats/network/identity/__init__.py
+++ cats/network/identity/__init__.py
@@ -6,6 +6,11 @@ from cats.network.identity.node_did import (
     node_did,
 )
 from cats.network.identity.node_uri import node_uri
+from cats.network.identity.webid import (
+    build_webid_document,
+    webid_uri,
+    write_webid_document,
+)
 
 __all__ = [
     'node_did',
@@ -13,4 +18,7 @@ __all__ = [
     'load_node_signing_material',
     'ed25519_public_key_to_did_key',
     'did_key_verification_method',
+    'webid_uri',
+    'build_webid_document',
+    'write_webid_document',
 ]
